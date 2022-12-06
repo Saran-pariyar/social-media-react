b213e75511fcab4c65ba5328a5f1787465eef131
Saran-pariyar <spariyar05@gmail.com>
2022-12-06 15:15:15 +0545

    Made message friend list using dummy api data

--- src/pages/Message/Message.jsx
+++ src/pages/Message/Message.jsx
@@ -1,12 +1,12 @@
 import "./Message.css";
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 
 const Message = () => {
-  const [friendMessage, setFriendMessage] = useState([])
+  const [friendMessage, setFriendMessage] = useState([]);
   useEffect(() => {
     fetch("https://dummyjson.com/users")
       .then((res) => res.json())
-      .then((data)=> setFriendMessage(data.users));
+      .then((data) => setFriendMessage(data.users));
   }, []);
   return (
     <div className="page-margin">
@@ -14,8 +14,18 @@ const Message = () => {
         <div className="message-left">
           <div className="user-title">saran_pariyar1</div>
           <div className="friends-container">
-            {friendMessage.map((friend)=>{
-return(<p>{friend.email} </p>)
+            {friendMessage.map((friend) => {
+              return (
+                <div className="message-person" key={friend.firstName}>
+                  <img src={friend.image} alt="" className="message-img" />
+                  <div className="text-section">
+                    <div className="name">
+                      {friend.firstName} {friend.lastName}
+                    </div>
+                    <div className="last-message">Active {friend.age} min ago</div>
+                  </div>
+                </div>
+              );
             })}
           </div>
         </div>
